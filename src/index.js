1ab55260f0c131535776db93a23039125660bbd4
Francesco Agosti <fra5@duke.edu>
2017-07-27 17:16:07 -0700

    Add fileToText and urlToText

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import { urlToPixels, fileToPixels, pixelsToText } from 'Src/utils'
+import { fileToText } from 'Src/utils'
 
 window.onload = () => {
 	const handleFileChange = function(e) {
 		const file = this.files[0]
-		fileToPixels(file, 250, .6).then((imgPixels) => {
-			const text = pixelsToText(imgPixels, `!function(t){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=t();`);
+		const text = `!function(t){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=t();`
+		const width = 200
+		const stretch = 0.6
+		fileToText(file, text, width, stretch)
+		.then((formattedText) => {
 			const content = document.getElementById("content");
-			content.innerHTML = text
+			content.innerHTML = formattedText
 		})
 	}	
 	const inputElement = document.getElementById("input");
--- src/utils.js
+++ src/utils.js
@@ -1,37 +1,14 @@
-import Promise from 'bluebird';
+import { imagePixelsPromise } from 'Src/ImagePixels.js';
 
-
-class ImagePixels {
-	constructor(img, width, yStretch) {
-		const aspectRatio = (img.height / img.width) * yStretch
-		this.width = width || img.width
-		this.height = this.width * aspectRatio
-		const canvas = document.createElement('canvas')
-		canvas.width = this.width
-		canvas.height = this.height
-		this.context = canvas.getContext('2d')
-		this.context.drawImage(img, 0, 0, this.width, this.height)
-	}
-
-	get(i, j) {
-		const data = this.context.getImageData(i, j, 1, 1).data
-		return {
-			r: data[0],
-			g: data[1],
-			b: data[2],
-			a: data[3] / 255,
-		}
-	}
+export const fileToText = (file, text, width, stretch, options) => {
+	return fileToPixels(file, width, stretch).then((imgPixels) => {
+		return pixelsToText(imgPixels, text, options)
+	})
 }
 
-const imagePixelsPromise = (img, width, stretch) => {
-	return new Promise((resolve, reject) => {
-		img.onload = () => {
-			resolve(new ImagePixels(img, width, stretch))
-		}
-		img.onerror = (err) => {
-			reject(err)
-		}
+export const urlToText = (url, text, width, stretch, options) => {
+	return urlToPixels(url, width, stretch).then((imgPixels) => {
+		return pixelsToText(imgPixels, text, options)
 	})
 }
 
