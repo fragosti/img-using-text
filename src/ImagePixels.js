4504e219c9371d9d4d6c1505768606ec71565c43
Francesco Agosti <fra5@duke.edu>
2017-07-27 17:41:40 -0700

    Add ImagePixels

---
+++ src/ImagePixels.js
@@ -0,0 +1,33 @@
+import Promise from 'bluebird';
+
+export default class ImagePixels {
+  constructor(img, width, yStretch) {
+    const aspectRatio = (img.height / img.width) * yStretch;
+    this.width = width || img.width;
+    this.height = this.width * aspectRatio;
+    const canvas = document.createElement('canvas');
+    canvas.width = this.width;
+    canvas.height = this.height;
+    this.context = canvas.getContext('2d');
+    this.context.drawImage(img, 0, 0, this.width, this.height);
+  }
+
+  get(i, j) {
+    const data = this.context.getImageData(i, j, 1, 1).data;
+    return {
+      r: data[0],
+      g: data[1],
+      b: data[2],
+      a: data[3] / 255,
+    };
+  }
+}
+
+export const imagePixelsPromise = (img, width, stretch) => new Promise((resolve, reject) => {
+  img.onload = () => {
+    resolve(new ImagePixels(img, width, stretch));
+  };
+  img.onerror = (err) => {
+    reject(err);
+  };
+});
