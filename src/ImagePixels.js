7c64907598f49945d4c41f378b3b317f2b198fd8
Francesco Agosti <francesco.agosti93@gmail.com>
2017-07-29 07:57:17 -0400

    Add documentation step and README

--- src/ImagePixels.js
+++ src/ImagePixels.js
@@ -1,6 +1,17 @@
 import Promise from 'bluebird';
 
+/**
+ * @private
+ * A class that wraps a canvas context and allows you to read individual pixels as rgba values. 
+ */ 
 export default class ImagePixels {
+  /**
+   * @private
+   * @param {Image} img 
+   * @param {number} [width = image width] width value if you want to resize the image
+   * @param {number} [yStretch = 1] how much you would like to stretch the height compared to the width
+   * @param {Canvas} [mockCanvas] mock canvas used only for testing with 'canvas-prebuilt'
+   */
   constructor(img, width, yStretch = 1, mockCanvas) {
     const aspectRatio = (img.height / img.width) * yStretch;
     this.width = width || img.width;
@@ -12,6 +23,13 @@ export default class ImagePixels {
     this.context.drawImage(img, 0, 0, this.width, this.height);
   }
 
+  /**
+   * @private
+   * Get an Object with keys r, g, b, a representing the pixel at point i, j
+   * @param {number} i 
+   * @param {number} j
+   * @returns {Object} with r, g, b, a values
+   */
   get(i, j) {
     const data = this.context.getImageData(i, j, 1, 1).data;
     return {
--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import Promise from 'bluebird';
 import { imagePixelsPromise } from './ImagePixels.js';
 
+/**
+ * Simple estimation for whether an rgba value is white or transparent
+ * @param {number} r red
+ * @param {number} g green
+ * @param {number} b blue
+ * @param {number} a alpha
+ * @returns {boolean} 
+ */
 export const isWhiteOrTransparent = (r, g, b, a) => (a < 0.1) || (r > 250 && g > 250 && b > 250);
 
+/**
+ * Convert from ImagePixels to text
+ * @param {ImagePixels} imgPixels instance of ImagePixels containing image information
+ * @param {Object} options options object
+ * @param {boolean} options.async whether to wrap the work for every row in a setTimeout
+ * @param {function} options.charForPixel the function to call to convert from an {r, g, b, a} object to a character or text. 
+ * @returns {string|Promise} the resulting text if not async, a Promise otherwise
+ */
 export const pixelsToText = (imgPixels, options) => {
   const { charForPixel, async } = Object.assign({}, {
     async: false,
@@ -42,6 +58,11 @@ export const pixelsToText = (imgPixels, options) => {
   return chars.join('');
 };
 
+/**
+ * Create an Image object from a File object
+ * @param {File} file the file (from an <input/> for example)
+ * @returns {Image}
+ */
 export const imageFromFile = (file) => {
   const img = new Image();
   img.file = file;
@@ -51,12 +72,26 @@ export const imageFromFile = (file) => {
   return img;
 };
 
+/**
+ * Given a File it returns an ImagePixels Promise
+ * @param {File} file
+ * @param {number} [width = image width] width value if you want to resize the image
+ * @param {number} [stretch = 1] how much you would like to stretch the height compared to the width
+ * @returns {Promise} a bluebird promise
+ */
 export const fileToPixels = (file, width, stretch) => {
   const img = imageFromFile(file);
   img.crossOrigin = 'Anonymous';
   return imagePixelsPromise(img, width, stretch);
 };
 
+/**
+ * Given a URL it returns an ImagePixels Promise
+ * @param {string} url url of the image (needs to allows CORS)
+ * @param {number} [width = image width] width value if you want to resize the image
+ * @param {number} [stretch = 1] how much you would like to stretch the height compared to the width
+ * @returns {Promise} a bluebird promise 
+ */
 export const urlToPixels = (url, width, stretch) => {
   const img = new Image();
   img.crossOrigin = 'Anonymous';
@@ -64,7 +99,27 @@ export const urlToPixels = (url, width, stretch) => {
   return imagePixelsPromise(img, width, stretch);
 };
 
+/**
+ * Given a File containing an image, convert it to a text representation.
+ * @param {File} file
+ * @param {number} [width = image width] width value if you want to resize the image
+ * @param {number} [stretch = 1] how much you would like to stretch the height compared to the width
+ * @param {Object} options options object
+ * @param {boolean} options.async whether to wrap the work for every row in a setTimeout
+ * @param {function} options.charForPixel the function to call to convert from an {r, g, b, a} object to a character or text. 
+ * @return {Promise} a promise that resolves to an ImagePixels instance
+ */
 export const fileToText = (file, width, stretch, options) => fileToPixels(file, width, stretch).then(imgPixels => pixelsToText(imgPixels, options));
 
+/**
+ * Given a File containing an image, convert it to a text representation.
+ * @param {string} url url of the image (needs to allows CORS)
+ * @param {number} [width = image width] width value if you want to resize the image
+ * @param {number} [stretch = 1] how much you would like to stretch the height compared to the width
+ * @param {Object} options options object
+ * @param {boolean} options.async whether to wrap the work for every row in a setTimeout
+ * @param {function} options.charForPixel the function to call to convert from an {r, g, b, a} object to a character or text. 
+ * @return {Promise} a promise that resolves to an ImagePixels instance
+ */
 export const urlToText = (url, width, stretch, options) => urlToPixels(url, width, stretch).then(imgPixels => pixelsToText(imgPixels, options));
 
